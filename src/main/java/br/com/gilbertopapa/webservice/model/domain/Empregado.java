b9434e4bab983d33d491b7ab307fd588d8570791
Gilberto Papa <gilberto.papa@digiage.com.br>
2019-05-03 14:30:11 -0300

    Persistindo o relacionamento

--- src/main/java/br/com/gilbertopapa/webservice/model/domain/Empregado.java
+++ src/main/java/br/com/gilbertopapa/webservice/model/domain/Empregado.java
@@ -1,4 +1,14 @@
 package br.com.gilbertopapa.webservice.model.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.ManyToMany;
+import java.util.List;
+
 public class Empregado {
+
+
+    @ManyToMany(mappedBy = "empregados")
+    @JsonIgnore
+    private List<Projeto> projetos;
 }
