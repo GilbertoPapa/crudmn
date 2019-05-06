66e44a0497c0eedee16ecb36629e1ef4ab0b41e8
Gilberto Papa <gilberto.papa@digiage.com.br>
2019-05-06 16:29:10 -0300

    Ajustando o Mapeamento

--- src/main/java/br/com/gilbertopapa/webservice/model/domain/Empregado.java
+++ src/main/java/br/com/gilbertopapa/webservice/model/domain/Empregado.java
@@ -2,11 +2,21 @@ package br.com.gilbertopapa.webservice.model.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import java.util.List;
 
 public class Empregado {
 
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String cargo;
 
     @ManyToMany(mappedBy = "empregados")
     @JsonIgnore
