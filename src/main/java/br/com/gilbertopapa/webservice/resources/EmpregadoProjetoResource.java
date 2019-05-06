3cf6e9d353faed1f6a39694d5798bdde768c25ae
Gilberto Papa <gilberto.papa@digiage.com.br>
2019-05-06 16:23:57 -0300

    Recuperando dados com web services

--- src/main/java/br/com/gilbertopapa/webservice/resources/EmpregadoProjetoResource.java
+++ src/main/java/br/com/gilbertopapa/webservice/resources/EmpregadoProjetoResource.java
@@ -1,4 +1,22 @@
 package br.com.gilbertopapa.webservice.resources;
 
+import br.com.gilbertopapa.webservice.model.domain.Empregado;
+import br.com.gilbertopapa.webservice.service.RelationshipService;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+
+@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 public class EmpregadoProjetoResource {
+
+    private RelationshipService service = new RelationshipService();
+    @GET
+    public List<Empregado> getEmpregados(@PathParam("projetoId")
+                                                 long projetoId) {
+        return service.getEmpregados(projetoId);
+    }
 }
