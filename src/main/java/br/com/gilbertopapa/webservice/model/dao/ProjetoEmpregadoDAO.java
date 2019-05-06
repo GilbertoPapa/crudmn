f1a60924e7d6cd74d655ccfca1e9cebf1df8418b
Gilberto Papa <gilberto.papa@digiage.com.br>
2019-05-06 17:01:44 -0300

    Ajustes persistência

--- src/main/java/br/com/gilbertopapa/webservice/model/dao/ProjetoEmpregadoDAO.java
+++ src/main/java/br/com/gilbertopapa/webservice/model/dao/ProjetoEmpregadoDAO.java
@@ -6,10 +6,51 @@ import br.com.gilbertopapa.webservice.model.domain.Empregado;
 import br.com.gilbertopapa.webservice.model.domain.Projeto;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.NoResultException;
+import java.util.List;
 
 public class ProjetoEmpregadoDAO {
+
+
+
+    public List<Empregado> getEmpregados(long projetoId) {
+        if(projetoId <= 0) {
+            throw new DAOException("O id do projeto precisa ser maior do que 0.", ErrorCode.BAD_REQUEST.getCode());
+        }
+
+        EntityManager em = JPAUtil.getEntityManager();
+
+        try {
+            return em.createQuery("select e from Empregado e JOIN e.projetos p where p.id = :projetoId", Empregado.class)
+                    .setParameter("projetoId", projetoId)
+                    .getResultList();
+        } catch (RuntimeException ex) {
+            throw new DAOException("Erro ao recuperar os empregados do projeto de id " + projetoId + " do banco: "
+                    + ex.getMessage(), ErrorCode.SERVER_ERROR.getCode());
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Projeto> getProjetos(long empregadoId) {
+        if(empregadoId <= 0) {
+            throw new DAOException("O id do empregado precisa ser maior do que 0.", ErrorCode.BAD_REQUEST.getCode());
+        }
+
+        EntityManager em = JPAUtil.getEntityManager();
+
+        try {
+            return em.createQuery("select p from Projeto p JOIN p.empregados e where e.id = :empregadoId", Projeto.class)
+                    .setParameter("empregadoId", empregadoId)
+                    .getResultList();
+        } catch (RuntimeException ex) {
+            throw new DAOException("Erro ao recuperar os projetos em que o empregado de id " + empregadoId +
+                    " está trabalhando: " + ex.getMessage(), ErrorCode.SERVER_ERROR.getCode());
+        } finally {
+            em.close();
+        }
+    }
+
 
     public void deleteRelationship(long projetoId , long empregadoId){
         if ((projetoId <= 0)||(empregadoId <=0)){
---
+++ src/main/java/br/com/gilbertopapa/webservice/service/RelationshipService.java
@@ -0,0 +1,28 @@
+package br.com.gilbertopapa.webservice.service;
+
+import br.com.gilbertopapa.webservice.model.dao.ProjetoEmpregadoDAO;
+import br.com.gilbertopapa.webservice.model.domain.Empregado;
+import br.com.gilbertopapa.webservice.model.domain.Projeto;
+
+import java.util.List;
+
+public class RelationshipService {
+
+    private ProjetoEmpregadoDAO dao = new ProjetoEmpregadoDAO();
+
+    public void saveRelationshipProjetoEmpregado(long projetoId, long empregadoId) {
+        dao.saveRelationship(projetoId, empregadoId);
+    }
+
+    public List<Empregado> getEmpregados(long projetoId) {
+        return dao.getEmpregados(projetoId);
+    }
+
+    public List<Projeto> getProjetos(long empregadoId) {
+        return dao.getProjetos(empregadoId);
+    }
+
+    public void deleteRelationshipProjetoEmpregado(long projetoId, long empregadoId) {
+        dao.deleteRelationship(projetoId, empregadoId);
+    }
+}
