3f7db7549f0f97d6df0d079ba3026cf6d45c4a4d
Gilberto Papa <gilberto.papa@digiage.com.br>
2019-05-06 10:58:36 -0300

    JPAUtil

---
+++ src/main/java/br/com/gilbertopapa/webservice/model/dao/JPAUtil.java
@@ -0,0 +1,20 @@
+package br.com.gilbertopapa.webservice.model.dao;
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+
+    private static EntityManagerFactory emf;
+
+    public static EntityManager getEntityManager() {
+        if (emf == null) {
+            emf = Persistence.createEntityManagerFactory("empresa");
+        }
+        return emf.createEntityManager();
+    }
+
+}
+
